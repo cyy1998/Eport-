44ef400ba46aa3e6bd397e714eb716d23d68c6a9
cyy1998 <caiyiyang1998@126.com>
2019-07-08 12:15:50 +0800

    3.0-homepage

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,32 +1,70 @@
 // pages/home/home.js
+const app=getApp();
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    id: '',
-    name: '',
-    type: '',
-    device: ''
+    userID: '',
+    userName: '',
+    usertype: '',
+    show_error_1: false,
+    show_error_2: false,
+    show_error_3: false,
+    isLogin: false,
+    showLogin: false,
+    is_password: true,
+    eye_status: 'browse',
+    animation: '',
+    error_message: '',
+    show_error: false
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    var _id=options['id'];
-    var _name=options['name'];
-    var _type=options['type'];
-    var _device=options['deviceID'];
-    console.log(_id+_name+_type);
-    this.setData({
-      id: _id,
-      name: _name,
-      type: _type,
-      device: _device,
-      swiperList: [1,2,3,4,5]
-    })
+    if (!app.globalData.loginStatus) {
+      var _id=wx.getStorageSync('id');
+      if(_id)
+      {
+        app.globalData.id=_id;
+      }
+      var _name=wx.getStorageSync('name');
+      if(_name)
+      {
+        app.globalData.name=_name;
+      }
+      var _type=wx.getStorageSync('type');
+      if(_type)
+      {
+        app.globalData.type=_type;
+      }
+      if (app.globalData.id != '') {
+        app.globalData.loginStatus = true;
+        this.setData({
+          userID: app.globalData.id,
+          userName: app.globalData.name,
+          userType: app.globalData.type,
+          isLogin: app.globalData.loginStatus
+        });
+      } else {
+        console.log('路人');
+        this.setData({
+          userName: '路人',
+          isLogin: app.globalData.loginStatus
+        });
+      }
+    } else {
+      console.log(app.globalData)
+      this.setData({
+        userID: app.globalData.id,
+        userName: app.globalData.name,
+        userType: app.globalData.type,
+        isLogin: app.globalData.loginStatus
+      });
+    }
   },
 
   /**
@@ -78,9 +116,108 @@ Page({
 
   },
 
-  quit: function() {
-    wx.reLaunch({
-      url: '../login/login?deviceID='+this.data.device
+  login: function(){
+    this.setData({
+      showLogin: true
     });
-  }
+  },
+  quit: function(){
+    app.globalData.loginStatus=false;
+    app.globalData.name='';
+    app.globalData.id='';
+    app.globalData.type='';
+    this.setData({
+      isLogin: false,
+      userName: '路人',
+      userID: '',
+      userType: '',
+      step: 'first'
+    });
+    wx.setStorage({
+      key: 'id',
+      data: ''
+    });
+    wx.setStorage({
+      key: 'name',
+      data: ''
+    });
+    wx.setStorage({
+      key: 'type',
+      data: ''
+    });
+  },
+  hideLogin: function(){
+    this.setData({
+      showLogin: false
+    });
+  },
+  formSubmit_lo: function (e) {
+    var that = this;
+    console.log(e.detail.value);
+    if (e.detail.value['id'] === '') {
+      that.setData({
+        animation: 'shake',
+        error_message: '用户名不能为空',
+        show_error: true
+      });
+      setTimeout(function () {
+        that.setData({
+          animation: ''
+        })
+      }, 1000);
+    } else if (e.detail.value['password'] === '') {
+      that.setData({
+        animation: 'shake',
+        error_message: '密码不能为空',
+        show_error: true
+      });
+      setTimeout(function () {
+        that.setData({
+          animation: ''
+        })
+      }, 1000);
+    } else {
+      app.globalData.name=e.detail.value['id'];
+      app.globalData.id=e.detail.value['id'];
+      app.globalData.type='巡检员';
+      app.globalData.loginStatus=true;
+      that.setData({
+        show_error: false,
+        showLogin: false,
+        isLogin: true,
+        userName: e.detail.value['id'],
+        userID: e.detail.value['id'],
+        userType: '巡检员'
+      });
+      wx.setStorage({
+        key: 'id',
+        data: app.globalData.id,
+        success: function(res)
+        {
+          console.log('yes');
+        }
+      });
+      wx.setStorage({
+        key: 'name',
+        data: app.globalData.name
+      });
+      wx.setStorage({
+        key: 'type',
+        data: app.globalData.type
+      });
+    }
+  },
+  changeStatus: function () {
+    if (this.data.is_password) {
+      this.setData({
+        is_password: false,
+        eye_status: "browse_fill"
+      });
+    } else {
+      this.setData({
+        is_password: true,
+        eye_status: "browse"
+      });
+    }
+  },
 })
